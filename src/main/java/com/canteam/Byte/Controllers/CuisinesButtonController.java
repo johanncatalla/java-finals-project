6fe4f60f9065ba8f73d14779a720f7d3a8077f5e
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-11-16 08:12:49 +0800

    Created fxml, controller, model for Cuisine Types

---
+++ src/main/java/com/canteam/Byte/Controllers/CuisinesButtonController.java
@@ -0,0 +1,28 @@
+package com.canteam.Byte.Controllers;
+
+import com.canteam.Byte.Models.CuisineModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
+public class CuisinesButtonController {
+
+    @FXML
+    private ImageView buttonImage;
+
+    @FXML
+    private Label cuisineName;
+
+    private CuisineModel cuisineModel;
+
+    public void setData(CuisineModel cuisineModel){
+        this.cuisineModel = cuisineModel;
+        cuisineName.setText(cuisineModel.getCuisineName());
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(cuisineModel.getCuisineImageSrc())));
+        buttonImage.setImage(image);
+    }
+
+}
---
+++ src/main/java/com/canteam/Byte/Models/CuisineModel.java
@@ -0,0 +1,22 @@
+package com.canteam.Byte.Models;
+
+public class CuisineModel {
+    private String cuisineName;
+    private String cuisineImageSrc;
+
+    public String getCuisineName() {
+        return cuisineName;
+    }
+
+    public void setCuisineName(String cuisineName) {
+        this.cuisineName = cuisineName;
+    }
+
+    public String getCuisineImageSrc() {
+        return cuisineImageSrc;
+    }
+
+    public void setCuisineImageSrc(String cuisineImageSrc) {
+        this.cuisineImageSrc = cuisineImageSrc;
+    }
+}

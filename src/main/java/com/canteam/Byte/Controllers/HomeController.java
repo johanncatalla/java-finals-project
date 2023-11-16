2845d159e69ab0830c32dae8cfdb859bd4a0db81
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-11-16 08:14:04 +0800

    Inserted the Cuisine Types section in Home

--- src/main/java/com/canteam/Byte/Controllers/HomeController.java
+++ src/main/java/com/canteam/Byte/Controllers/HomeController.java
@@ -1,20 +1,26 @@
 package com.canteam.Byte.Controllers;
+import com.canteam.Byte.Models.CuisineModel;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.util.Duration;
 import javafx.scene.image.Image;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class HomeController {
+public class HomeController implements Initializable{
 
     @FXML
     private Button burgerCloseIcon, burgerOpenIcon;
@@ -32,6 +38,26 @@ public class HomeController {
     protected ImageView testImageView;
 
     @FXML
+    private GridPane cuisinesGridPane;
+
+    private List<CuisineModel> cuisineList = new ArrayList<>();
+
+    private List<CuisineModel> getData(){
+      List<CuisineModel> cuisineList = new ArrayList<>();
+      CuisineModel cuisine;
+
+      // Just a demo/placeholder data for now
+      for (int i = 0; i < 12; i++){
+          cuisine = new CuisineModel();
+          cuisine.setCuisineName("Cuisine " + i);
+          cuisine.setCuisineImageSrc("/com/canteam/Byte/assets/images/CuisineType/Milktea.png");
+          cuisineList.add(cuisine);
+      }
+      return cuisineList;
+    }
+
+
+    @FXML
     protected void onBurgerOpenIconClicked(){
         TranslateTransition burgerMenuTransition = new TranslateTransition();
         burgerMenuTransition.setNode(burgerMenuPane);
@@ -47,11 +73,39 @@ public class HomeController {
         burgerMenuTransition.setToX(0);
         burgerMenuTransition.setDuration(Duration.seconds(.5));
         burgerMenuTransition.play();
-        File file = new File("src/main/java/com/canteam/Byte/Controllers/kat.jpg");
-        Image image = new Image(file.toURI().toString());
-        testImageView.setImage(image);
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Will create an error at start but will be ok when you open the home page
+        cuisineList.addAll(getData());
+        int column = 1;
+        int row = 0;
+        try {
+            for (int i = 0; i < cuisineList.size(); i++){
+                // Get the fxml file for the cuisine button
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("/com/canteam/Byte/fxml/CuisinesButton.fxml"));
+                AnchorPane cuisineButton = fxmlLoader.load();
+
+                // Set the data for the cuisine button
+                CuisinesButtonController cuisinesButtonController = fxmlLoader.getController();
+                cuisinesButtonController.setData(cuisineList.get(i));
+
+                // Place the cuisine button in the gridPane
+                if (row == 2){
+                    row = 0;
+                    column++;
+                }
+                cuisinesGridPane.add(cuisineButton, column, row++);
+
+                // Set the margin for the cuisine button
+                GridPane.setMargin(cuisineButton, new Insets(10));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 

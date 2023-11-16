28783aa41629a2c18827f9481e87450da45eb6bb
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-11-17 07:25:06 +0800

    Created a method to enable window dragging

--- src/main/java/com/canteam/Byte/Controllers/Draggable.java
+++ src/main/java/com/canteam/Byte/Controllers/Draggable.java
@@ -4,8 +4,13 @@ import javafx.scene.Node;
 
 public class Draggable {
     private double mouseAnchorX;
+    private double mouseAnchorY;
 
-    public void makeDraggable(Node node){
+    /**
+     *  This method makes a node draggable on the X axis
+     * @param node The node to be made draggable
+     */
+    public void makeDraggableX(Node node){
         node.setOnMousePressed(mouseEvent -> {
             mouseAnchorX = mouseEvent.getSceneX();
         });
@@ -15,4 +20,49 @@ public class Draggable {
         });
     }
 
+    /**
+     * This method makes a node draggable on the Y axis
+     * @param node The node to be made draggable
+     */
+    public void makeDraggableY(Node node){
+        node.setOnMousePressed(mouseEvent -> {
+            mouseAnchorY = mouseEvent.getSceneY();
+        });
+
+        node.setOnMouseDragged(mouseEvent -> {
+            node.setLayoutY(mouseEvent.getSceneY() - mouseAnchorY);
+        });
+    }
+
+    /**
+     * This method makes a node draggable on the X and Y axis
+     * @param node The node to be made draggable
+     */
+    public void makeDraggableXY(Node node){
+        node.setOnMousePressed(mouseEvent -> {
+            mouseAnchorX = mouseEvent.getSceneX();
+            mouseAnchorY = mouseEvent.getSceneY();
+        });
+
+        node.setOnMouseDragged(mouseEvent -> {
+            node.setLayoutX(mouseEvent.getSceneX() - mouseAnchorX);
+            node.setLayoutY(mouseEvent.getSceneY() - mouseAnchorY);
+        });
+    }
+
+    /**
+     * This method makes the parent node (window) draggable on the X and Y axis
+     * @param node The node to be made draggable
+     */
+    public void makeWindowDraggable(Node node){
+        node.setOnMousePressed(mouseEvent -> {
+            mouseAnchorX = mouseEvent.getSceneX();
+            mouseAnchorY = mouseEvent.getSceneY();
+        });
+
+        node.setOnMouseDragged(mouseEvent -> {
+            node.getScene().getWindow().setX(mouseEvent.getScreenX() - mouseAnchorX);
+            node.getScene().getWindow().setY(mouseEvent.getScreenY() - mouseAnchorY);
+        });
+    }
 }

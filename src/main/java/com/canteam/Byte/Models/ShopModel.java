34f08c9ae583c17b826e7ebc424134d7a0c671a4
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-11-19 08:49:43 +0800

    Created base ShopModel

--- src/main/java/com/canteam/Byte/Models/ShopModel.java
+++ src/main/java/com/canteam/Byte/Models/ShopModel.java
@@ -1,4 +1,21 @@
 package com.canteam.Byte.Models;
 
 public class ShopModel {
+    String shopName, shopImageSrc;
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getShopImageSrc() {
+        return shopImageSrc;
+    }
+
+    public void setShopImageSrc(String shopImageSrc) {
+        this.shopImageSrc = shopImageSrc;
+    }
 }

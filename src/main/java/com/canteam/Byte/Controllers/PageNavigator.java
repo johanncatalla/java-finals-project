37077f42c794fb34ee7994ad0f74185956d88357
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2023-11-22 00:11:46 +0800

    Added stack for navigation history and forward backward navigation methods

--- src/main/java/com/canteam/Byte/Controllers/PageNavigator.java
+++ src/main/java/com/canteam/Byte/Controllers/PageNavigator.java
@@ -7,9 +7,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 class PageNavigator{
+
+    // Stacks for Navigation History
+    private static ArrayList<String> navigationHistory = new ArrayList<>();
+
+    public void push(String pageName){
+        navigationHistory.add(pageName);
+        System.out.println(navigationHistory.toString());
+    }
+
+    public String pop(){
+        String destination = navigationHistory.get(navigationHistory.size() - 1);
+        navigationHistory.remove(navigationHistory.size()-1);
+        System.out.println(navigationHistory.toString());
+        return destination;
+    }
 
     /**
      * Navigates to a page
@@ -30,14 +46,34 @@ class PageNavigator{
             }
     }
 
+    public void forwardToPage(Node node,String sourcePageName, String destinationPageName){
+        push(sourcePageName);
+        navigateToPage(node, destinationPageName);
+    }
+
+    public void backToPage(Node node){
+        navigateToPage(node, pop());
+    }
+
     /**
      * Makes a node clickable and navigates to a page
      * @param node The node to be clicked
-     * @param pageName The name of the page to be navigated to
+     * @param sourcePageName The name of the current page
+     * @param destinationPageName The name of the destination page.
      */
-    public void makeNavigator(Node node, String pageName){
-        node.setOnMouseClicked(mouseEvent ->{
-            navigateToPage(node, pageName);
+     public void makeForwardNavigator(Node node, String sourcePageName, String destinationPageName){
+        node.setOnMouseClicked(mouseEvent -> {
+            forwardToPage(node, sourcePageName, destinationPageName);
+        });
+    }
+
+    /**
+     * Makes a node clickable and navigates back a page
+     * @param node The node to be clicked
+     */
+    public void makeBackwardNavigator(Node node){
+        node.setOnMouseClicked(mouseEvent -> {
+            backToPage(node);
         });
     }
 }

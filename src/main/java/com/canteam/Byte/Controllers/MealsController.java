e26161c5941315da728765790abb266ab21a2589
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2023-11-22 22:51:46 +0800

    Updated meals css, fxml, and controllers

--- src/main/java/com/canteam/Byte/Controllers/MealsController.java
+++ src/main/java/com/canteam/Byte/Controllers/MealsController.java
@@ -1,4 +1,47 @@
 package com.canteam.Byte.Controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 public class MealsController {
+
+    @FXML
+    private Button addQtyBtn;
+
+    @FXML
+    private Button addToCartBtn;
+
+    @FXML
+    private Button btnClose;
+
+    @FXML
+    private CheckBox chkExtraRice;
+
+    @FXML
+    private GridPane extrasGridPane;
+
+    @FXML
+    private ImageView mealImg;
+
+    @FXML
+    private Label mealName;
+
+    @FXML
+    private Label mealPrice;
+
+    @FXML
+    private Label qtyLabel;
+
+    @FXML
+    private TextField specialInstructionsTxt;
+
+    @FXML
+    private Button subtractQtyBtn;
+
+
+
 }

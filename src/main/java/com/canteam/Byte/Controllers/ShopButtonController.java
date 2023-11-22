0c9180d9267a4ada6a0520559f45a234238700cb
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2023-11-22 10:50:42 +0800

    Modified shop button

--- src/main/java/com/canteam/Byte/Controllers/ShopButtonController.java
+++ src/main/java/com/canteam/Byte/Controllers/ShopButtonController.java
@@ -2,20 +2,29 @@ package com.canteam.Byte.Controllers;
 
 import com.canteam.Byte.Models.ShopModel;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class ShopButtonController {
+public class ShopButtonController implements Initializable {
     @FXML
     private ImageView buttonImage;
 
     @FXML
     private Label shopName;
 
+    @FXML
+    private AnchorPane shopButton;
+
     private ShopModel shopModel;
+
+    PageNavigator pageNavigator = new PageNavigator();
 
     public void setData(ShopModel shopModel){
         this.shopModel = shopModel;
@@ -24,5 +33,12 @@ public class ShopButtonController {
         buttonImage.setImage(image);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        shopButton.setOnMouseClicked(mouseEvent -> {
+            ShopModel.setSelectedShopName(shopModel.getShopName());
+            pageNavigator.makeForwardNavigator(shopButton, "Restaurants", "RestaurantMenu");
+        });
+    }
 }
 

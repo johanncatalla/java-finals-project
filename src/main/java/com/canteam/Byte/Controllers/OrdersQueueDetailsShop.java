739b76203f80b2edc8892c7c6facd90780c6e763
Princess Nicole Oriola <pnc.oriola@gmail.com>
2023-11-22 20:40:10 +0800

    shop version

---
+++ src/main/java/com/canteam/Byte/Controllers/OrdersQueueDetailsShop.java
@@ -0,0 +1,16 @@
+package com.canteam.Byte.Controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OrdersQueueDetailsShop extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}

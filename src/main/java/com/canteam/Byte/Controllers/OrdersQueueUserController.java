281652c054c0da8d58fb93ea71b54f98f7060346
Johann Sebastian Catalla <catallajohann@gmail.com>
2023-11-23 11:52:54 +0800

    Added orders fxml and controller

---
+++ src/main/java/com/canteam/Byte/Controllers/OrdersQueueUserController.java
@@ -0,0 +1,30 @@
+package com.canteam.Byte.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OrdersQueueUserController implements Initializable {
+
+    @FXML
+    private Button btnViewOrder;
+
+    @FXML
+    private Label txtOrderItems;
+
+    @FXML
+    private Label txtOrderShop;
+
+    @FXML
+    private Label txtOrderStatus;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // get data
+
+    }
+}
